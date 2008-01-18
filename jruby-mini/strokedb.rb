85c8e319ddd433750fa71b5c71380a9e4aaa44ca
oleganza <oleganza@gmail.com>
2008-01-19 00:22:51 +0300

    added infile test guard

--- jruby-mini/strokedb.rb
+++ jruby-mini/strokedb.rb
@@ -157,27 +157,29 @@ module StrokeDB
   end
 
 end
-#### test
 
-store = StrokeDB::FileStore.new "db"
-_d = nil
-100.times do |i|
-  _d1 = store.new_doc :welcome => 1
-  _d = store.new_doc :hello => "once#{i}", :__meta__ => "Beliberda", :_d1 => _d1
-  _d.save!
-  _d1.save!
+if __FILE__ == $0
+  store = StrokeDB::FileStore.new "db"
+  _d = nil
+  100.times do |i|
+    _d1 = store.new_doc :welcome => 1
+    _d = store.new_doc :hello => "once#{i}", :__meta__ => "Beliberda", :_d1 => _d1
+    _d.save!
+    _d1.save!
+  end
+
+  puts "last saved:"
+  d_ = store.find(_d.uuid)
+  puts store.send!(:filename,_d.uuid)
+  puts d_
+  d_[:something] = 1
+  d_.save!
+  puts d_
+  puts "----"
+  d_[:something_else] = 2
+  d_.save!
+  puts d_
+  puts d_[:_d1]
+  puts d_.previous_versions.inspect
 end
 
-puts "last saved:"
-d_ = store.find(_d.uuid)
-puts store.send!(:filename,_d.uuid)
-puts d_
-d_[:something] = 1
-d_.save!
-puts d_
-puts "----"
-d_[:something_else] = 2
-d_.save!
-puts d_
-puts d_[:_d1]
-puts d_.previous_versions.inspect
