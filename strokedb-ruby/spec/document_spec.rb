f115d7bc06b40762eea4f0ba4c48edd27a9d1a0d
Yurii Rashkovskii <yrashk@gmail.com>
2008-01-22 04:52:06 +0200

    Minor spec fix

--- strokedb-ruby/spec/document_spec.rb
+++ strokedb-ruby/spec/document_spec.rb
@@ -108,7 +108,7 @@ describe "Invalid Document's JSON (i.e. incorrect __version__)" do
     @json = @document.to_json.gsub(@document.version,'incorrect version')
   end
   
-  it "should be loadable into Document" do
+  it "should not be loadable into Document" do
     lambda do
       StrokeDB::Document.from_json(@store,'7bb032d4-0a3c-43fa-b1c1-eea6a980452d',@json)
     end.should raise_error(StrokeDB::VersionMismatchError)
