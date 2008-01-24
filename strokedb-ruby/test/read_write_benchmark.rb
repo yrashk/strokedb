0f6d0ceed7f4227ce38b991562841ad7ef162d00
oleganza <oleganza@gmail.com>
2008-01-24 19:18:36 +0300

    added read tests

--- strokedb-ruby/test/read_write_benchmark.rb
+++ strokedb-ruby/test/read_write_benchmark.rb
@@ -15,18 +15,43 @@ def test_storage(bm, n, title, &block)
   end
 end
 
-N = 128
+N = 1024
+M = 10
 bm(28) do |x| 
-  test_storage x, N, "Write                       " do |i|
+  test_storage x, N/100, "Write (#{N/100} documents)       " do |i|
     d = store.new_doc :index => i
     d.save!
   end
   
   # init and enable the cache
   $storage.chunks_cache = {}
-  
-  test_storage x, N, "Write (FileChunkStore cache)" do |i|
+  some_random_uuids = []
+  all_docs = []
+  test_storage x, N,     "Write (#{N} with cache)     " do |i|
     d = store.new_doc :index => i
     d.save!
+    some_random_uuids << d.uuid if some_random_uuids.size < M
+    all_docs << d.uuid
   end
+  
+  $storage.flush!
+  $storage.chunks_cache = {}  
+  
+  GC.start
+  x.report(          "Read (#{M} docs #{N} times) ") do
+    N.times do
+      some_random_uuids.each do |uuid|
+        store.find(uuid)
+      end
+    end
+  end
+  GC.start
+  x.report(          "Read (#{N} docs #{M} times) ") do
+    M.times do
+      all_docs.each do |uuid|
+        store.find(uuid)
+      end
+    end
+  end
+  
 end
