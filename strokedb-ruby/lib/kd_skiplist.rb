c10fbeb2cf6a5b4e1aa6eb46b2134987310221e9
oleganza <oleganza@gmail.com>
2008-01-25 10:14:57 +0300

    added insertion into kd skiplist v2

--- strokedb-ruby/lib/kd_skiplist.rb
+++ strokedb-ruby/lib/kd_skiplist.rb
@@ -13,12 +13,29 @@ module StrokeDB
     #
     def initialize(dimensions)
       @dimensions = dimensions
+      @lists = @dimensions.map{ Skiplist.new() }
     end
     
     # Inserts some object to a k-d list.
     # Relies on a #[] method to access dimensions slots.
-    def insert(slots, __cheater_levels = nil)
+    def insert(object, __cheater_levels = nil)
+      locations = []
+      k = @dimensions.size
       
+      # Allocation nodes per each dimension
+      @dimensions.each_with_index do |d, i|
+        node = Node.new(object)
+        locations[i] = node
+        @lists[i].insert(object[d], node, __cheater_levels ? __cheater_levels[d] : nil)
+      end
+      
+      # Add references to all nodes to their copies in other dimensions
+      @dimensions.each_with_index do |d, i1|
+        0.upto(k - 2) do |j|
+          i2 = (i1 + j + 1) % k
+          locations[i1].pointers[i2] = locations[i2]
+        end
+      end
     end
     
     # Find a subset of data in a multidimensional range.
@@ -43,6 +60,24 @@ module StrokeDB
       
     end
     
+    def to_s
+      "#<#{self.class.name} " +
+      "@dimensions=#{@dimensions} " +
+      "@lists=[#{@lists.join(", ")}]>"
+    end
+    
+    class Node
+      attr_accessor :data, :pointers
+      def initialize(data)
+        @data = data
+        @pointers = []
+      end
+      def to_s
+        "#<KDNode " +
+        "@data=#{@data.inspect}" +
+        ">"
+      end
+    end
       
   end
   
