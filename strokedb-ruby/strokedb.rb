fd06f13ff776c93e307477ab8812dec625fd5ca1
Yurii Rashkovskii <yrashk@gmail.com>
2008-01-26 10:13:16 +0200

    first upcoming version will be 0.0.1

--- strokedb-ruby/strokedb.rb
+++ strokedb-ruby/strokedb.rb
@@ -20,6 +20,6 @@ require 'activesupport'
  [RUBY_PLATFORM =~ /java/ ? 'java_util' : nil ]).compact.each {|m| require File.dirname(__FILE__) + "/lib/#{m}"}
 
 module StrokeDB
-  VERSION = '0.1' + (RUBY_PLATFORM =~ /java/ ? 'j' : '')
+  VERSION = '0.0.1' + (RUBY_PLATFORM =~ /java/ ? '-java' : '')
   UUID_RE = /([a-f0-9]{8}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{12})/
 end
