669571def1116efbbb6aa513b4d92167cc8a32db
Yurii Rashkovskii <yrashk@gmail.com>
2008-01-26 14:20:37 +0200

    Extracted ChunkStorage spec from FileChunkStorage and MemoryChunkStorage specs

---
+++ strokedb-ruby/spec/chunk_storage_spec.rb
@@ -0,0 +1,25 @@
+require File.dirname(__FILE__) + '/spec_helper'
+
+describe "ChunkStorage", :shared => true do
+
+  it "should find a chunk by UUID" do
+    @storage.find(@chunk.uuid).should be_nil
+    @storage.save! @chunk
+    @storage.find(@chunk.uuid).should be_eql(@chunk)
+    @storage.find(@chunk.uuid).to_raw.should == @chunk.to_raw
+  end
+
+  it "should find a MASTER chunk" do
+    @storage.find('MASTER').should be_nil
+    c = Chunk.new(99)
+    raw_doc = {'ref' => 'chunk'}
+    c.uuid = 'MASTER'
+    c.insert('some-uuid', raw_doc)
+    @storage.save! c
+    @storage.find('MASTER').find('some-uuid').should == raw_doc
+  end
+
+
+
+end
+
--- strokedb-ruby/spec/memory_chunk_storage_spec.rb
+++ strokedb-ruby/spec/memory_chunk_storage_spec.rb
@@ -9,34 +9,7 @@ describe "MemoryChunkStorage" do
     @chunk.insert('34b030ab-03a5-a08a-4d97-a7b27daf0897', {'a' => 1, 'b' => 2})
   end
 
-  it "should be empty when created" do
-    # TODO
-  end
-
-  # it "should save something" do
-  #   @storage.save! @chunk
-  #   @storage.each do |chunk|
-  #     chunk.should be_eql(@chunk)
-  #   end
-  # end
-
-  it "should find a chunk by UUID" do
-    @storage.find(@chunk.uuid).should be_nil
-    @storage.save! @chunk
-    @storage.find(@chunk.uuid).should be_eql(@chunk)
-    @storage.find(@chunk.uuid).to_raw.should == @chunk.to_raw
-  end
-
-  it "should find a MASTER chunk" do
-    @storage.find('MASTER').should be_nil
-    c = Chunk.new(99)
-    raw_doc = {'ref' => 'chunk'}
-    c.uuid = 'MASTER'
-    c.insert('some-uuid', raw_doc)
-    @storage.save! c
-    @storage.find('MASTER').find('some-uuid').should == raw_doc
-  end
-
+  it_should_behave_like "ChunkStorage"
 
 
 end
