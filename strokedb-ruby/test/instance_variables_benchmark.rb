63dc22b169348de3174ec2ee4e5a7b3a3980ad02
oleganza <oleganza@gmail.com>
2008-01-28 01:42:08 +0300

    added instance variables benchmark

---
+++ strokedb-ruby/test/instance_variables_benchmark.rb
@@ -0,0 +1,34 @@
+
+require 'benchmark'
+include Benchmark 
+
+N = 10
+M = 100_000
+
+class A
+  def initialize
+    @var = Object.new
+  end
+  def access_ivar
+    N.times {
+      a = @var
+    }
+  end
+  def access_var
+    var = @var
+    N.times {
+      a = var
+    }
+  end
+end
+
+bm(24) do |x| 
+  x.report("Accessing @var") do
+    a = A.new
+    M.times { a.access_ivar }
+  end
+  x.report("Accessing var = @var") do
+    a = A.new
+    M.times { a.access_var }
+  end
+end
