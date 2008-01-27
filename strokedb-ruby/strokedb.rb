63a6de8ec67874686c43f6775cbf81a7a6b581f6
oleganza <oleganza@gmail.com>
2008-01-27 07:28:02 +0300

    refactored files layout

--- strokedb-ruby/strokedb.rb
+++ strokedb-ruby/strokedb.rb
@@ -2,21 +2,21 @@ require 'rubygems'
 require 'activesupport'
 
 (%w[
-  util 
-  skiplist
-  kd_skiplist
-  slot
-  document
-  diff
-  packet
-  store
-  skiplist_store
-  chunk
-  chunk_storage
-  memory_chunk_storage
-  file_chunk_storage
+  util/util 
+  data_structures/skiplist
+  data_structures/kd_skiplist
+  document/slot
+  document/document
+  sync/diff
+  sync/packet
+  stores/store
+  stores/skiplist_store
+  stores/skiplist_store/chunk
+  stores/skiplist_store/chunk_storage
+  stores/skiplist_store/memory_chunk_storage
+  stores/skiplist_store/file_chunk_storage
   ] +
- [RUBY_PLATFORM =~ /java/ ? 'java_util' : nil ]).compact.each {|m| require File.dirname(__FILE__) + "/lib/#{m}"}
+ [RUBY_PLATFORM =~ /java/ ? 'util/java_util' : nil ]).compact.each {|m| require File.dirname(__FILE__) + "/lib/#{m}"}
 
 module StrokeDB
   VERSION = '0.0.1' + (RUBY_PLATFORM =~ /java/ ? '-java' : '')
