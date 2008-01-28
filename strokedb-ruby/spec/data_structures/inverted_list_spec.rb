7cbcb429730038a1aa9c085be401efe2404de91d
oleganza <oleganza@gmail.com>
2008-01-29 00:42:02 +0300

    InvertedList: added numeric spec

--- strokedb-ruby/spec/data_structures/inverted_list_spec.rb
+++ strokedb-ruby/spec/data_structures/inverted_list_spec.rb
@@ -2,16 +2,6 @@ require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
  
 describe InvertedList, " with flat string attributes" do
   
-  def new_doc(meta, slots)
-    slots[:__meta__] = meta
-    slots[:__version__] = 'v1' + rand(10000000).to_s
-    {:uuid => meta + rand(1000000).to_s, :slots => slots}
-  end
-  
-  def insert_doc(il, doc)
-    il.insert(doc[:slots], doc[:uuid])
-  end
-  
   before(:all) do
     @il = InvertedList.new
     @oleg_profile   = new_doc('Profile', :name   => 'Oleg',
@@ -81,4 +71,56 @@ describe InvertedList, " with flat string attributes" do
              ).should == [   ].to_set
   end
   
+end
+
+describe InvertedList, " with numeric attributes" do
+  
+  before(:all) do
+    @il = InvertedList.new
+    @ps = []
+    @ps << new_doc('Point', :x =>    0, :y =>  0)   # 0 
+    @ps << new_doc('Point', :x =>   10, :y =>  50)  # 1
+    @ps << new_doc('Point', :x =>   50, :y =>  50)  # 2
+    @ps << new_doc('Point', :x =>  200, :y =>  10)  # 3
+    @ps << new_doc('Point', :x =>  500, :y =>  10)  # 4
+    @ps << new_doc('Point', :x => -500, :y =>  10)  # 5
+    @ps << new_doc('Point', :x =>  -20, :y =>  10)  # 6
+    @ps << new_doc('Point', :x => -2.1, :y =>  10)  # 7
+    @ps << new_doc('Point', :x => 20.6, :y =>  10)  # 8
+    
+    @ps.each {|p| insert_doc(@il, p) }
+  end
+  
+  it "should find by positive value" do
+    @il.find(:x =>  10).should == [@ps[1][:uuid]].to_set
+    @il.find(:x =>  50).should == [@ps[2][:uuid]].to_set
+    @il.find(:x => 200).should == [@ps[3][:uuid]].to_set
+  end
+  
+  it "should find by negative value" do
+    @il.find(:x => -500).should == [@ps[5][:uuid]].to_set
+    @il.find(:x =>  -20).should == [@ps[6][:uuid]].to_set
+  end
+
+  it "should find by zero value" do
+    @il.find(:x => 0).should == [@ps[0][:uuid]].to_set
+  end
+  
+  # Dangerous: 2.1 may suddenly appear as 2.0999999999996235 or like that
+  it "should find by float value" do
+    @il.find(:x => -2.1).should == [@ps[7][:uuid]].to_set
+    @il.find(:x => 20.6).should == [@ps[8][:uuid]].to_set
+  end
+  
+  
+end
+
+def new_doc(meta, slots)
+  slots[:__meta__] = meta
+  slots[:__version__] = 'v1' + rand(10000000).to_s
+  {:uuid => meta + rand(1000000).to_s, :slots => slots}
+end
+
+def insert_doc(il, doc)
+  il.insert(doc[:slots], doc[:uuid])
 end
