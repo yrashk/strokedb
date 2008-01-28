c3d1da2cebf49e18feac87d0f72687a33c7b6eb2
oleganza <oleganza@gmail.com>
2008-01-28 22:39:17 +0300

    InvertedList: added multiattribute search

--- strokedb-ruby/lib/data_structures/inverted_list.rb
+++ strokedb-ruby/lib/data_structures/inverted_list.rb
@@ -16,28 +16,63 @@ module StrokeDB
   	  slots.each do |key, value|
   	    value = value.to_s
   	    key = key.to_s
-  	    @list.insert(value + SEPARATOR + key + TERMINATOR, data)
+  	    prefix = value + SEPARATOR + key + TERMINATOR
+  	    #debug "Inserting prefix #{prefix.inspect} with #{data.inspect}"
+  	    @list.insert(prefix, data)
+  	    #debug "Now list is #{@list}"
 	    end
   	end
   	
   	def find(*args)
   	  q = PointQuery.new(*args)
-  	  
-  	  disjunction = []
+  	  total = Set.new
+  	  first_pass = true
+  	  #debug_header
+  	  #debug "Search #{q.slots.inspect}..."
+  	  #debug @list.to_s
   	  q.slots.each do |key, value|
   	    results = []
   	    key = key.to_s
   	    value = value.to_s
   	    prefix = value + SEPARATOR + key + TERMINATOR
+  	    #debug "Looking for prefix #{prefix.inspect}"
   	    node = @list.find_nearest_node(prefix)
   	    while node.key == prefix
   	      results.push node.value
   	      node = node.next
   	    end
-  	    disjunction += results
-  	    disjunction.uniq!
+  	    #debug "Results: #{results.inspect}; first pass? #{first_pass}"
+  	    total = (first_pass ? results.to_set : (total & results))
+  	    first_pass = false
+  	    #debug "intermediate total = #{total.inspect}"
 	    end
-	    disjunction
+	    #debug "returning total = #{total.inspect}"
+	    total
   	end
+  	
+  	def debug(msg)
+      if block_given?
+        begin
+          out = []
+          out << "\n\n---- Start of #{msg} -----"
+          yield(out)
+          return
+        rescue => e
+          puts out.join("\n")
+          puts "---- End of #{msg}: exception! -----"
+          puts e
+          puts e.backtrace.join("\n") rescue nil
+          puts "----"
+          raise e
+        end
+      else
+        puts "IL DEBUG: #{msg}" if ENV['DEBUG']
+      end
+    end
+    def debug_header
+      puts "\n==========================================\n" if ENV['DEBUG']
+    end
+    
+  	
 	end
 end
--- strokedb-ruby/spec/data_structures/inverted_list_spec.rb
+++ strokedb-ruby/spec/data_structures/inverted_list_spec.rb
@@ -5,7 +5,7 @@ describe InvertedList, " with flat string attributes" do
   def new_doc(meta, slots)
     slots[:__meta__] = meta
     slots[:__version__] = 'v1' + rand(10000000).to_s
-    {:uuid => rand(1000000).to_s, :slots => slots}
+    {:uuid => meta + rand(1000000).to_s, :slots => slots}
   end
   
   def insert_doc(il, doc)
@@ -14,25 +14,50 @@ describe InvertedList, " with flat string attributes" do
   
   before(:all) do
     @il = InvertedList.new
-    @oleg_profile   = new_doc('Profile', :name  => 'Oleg',
-                                         :email => 'oleganza')
-    @yrashk_profile = new_doc('Profile', :name  => 'Yurii', 
-                                         :email => 'yrashk')
-    @article1       = new_doc('Article', :title => 'StrokeDB kicks ass', 
+    @oleg_profile   = new_doc('Profile', :name   => 'Oleg',
+                                         :email  => 'oleganza')
+    @yrashk_profile = new_doc('Profile', :name   => 'Yurii', 
+                                         :email  => 'yrashk')
+    @article1       = new_doc('Article', :title  => 'StrokeDB kicks ass', 
                                          :author => ('#@' + @yrashk_profile[:uuid]))
-    @article2       = new_doc('Article', :title => 'StrokeDB strikes back', 
+    @article2       = new_doc('Article', :title  => 'StrokeDB strikes back', 
+                                         :date   => '28 Jan 2008',
                                          :author => ('#@' + @yrashk_profile[:uuid]))
+    @post1          = new_doc('Post',    :title  => 'Hello', 
+                                         :date   => '28 Jan 2008',
+                                         :author => ('#@' + @yrashk_profile[:uuid]))
+    
     insert_doc(@il, @oleg_profile)
     insert_doc(@il, @yrashk_profile)
     insert_doc(@il, @article1)
     insert_doc(@il, @article2)
+    insert_doc(@il, @post1)
   end
   
   it "should find objects by a single attribute" do
-    @il.find(:name => 'Oleg').should == [@oleg_profile[:uuid]]
-    @il.find(:email => 'yrashk').should == [@yrashk_profile[:uuid]]
-    @il.find(:__meta__ => 'Article').to_set.should == [@article1[:uuid], @article2[:uuid]].to_set
-    @il.find(:__version__ => @article1[:slots][:__version__]).to_set.should == [@article1[:uuid]].to_set
+    @il.find(:name => 'Oleg').should == [@oleg_profile[:uuid]].to_set
+    @il.find(:email => 'yrashk').should == [@yrashk_profile[:uuid]].to_set
+    @il.find(:__meta__ => 'Article').should == [@article1[:uuid], @article2[:uuid]].to_set
+    @il.find(:__version__ => @article1[:slots][:__version__]).should == [@article1[:uuid]].to_set
   end
+  
+  it "should not find object by a non-matched attribute" do
+    @il.find(:name => 'Nobody').should == [  ].to_set
+    @il.find(:__meta__ => 'NoMeta').should == [  ].to_set
+    @il.find(:__version__ => 'no-version').should == [  ].to_set
+  end
+  
+  it "should find objects by a pair of attributes" do
+    @il.find(:date => '28 Jan 2008', :title => 'Hello').should == [@post1[:uuid]].to_set
+    @il.find(:date => '28 Jan 2008', :__meta__ => 'Article').should == [@article2[:uuid]].to_set
+    @il.find(:date => '28 Jan 2008', :author => ('#@' + @yrashk_profile[:uuid])).should == [@post1[:uuid], @article2[:uuid]].to_set
+  end
+  
+  it "should not find objects by a not-matched pair of attributes" do
+    @il.find(:date => '28 Jan 2008', :title => 'StrokeDB kicks ass').should == [  ].to_set
+    @il.find(:date => '28 Jan 2008', :__meta__ => 'Profile').should == [  ].to_set
+    @il.find(:date => '28 Jan 2008', :author => ('#@' + @oleg_profile[:uuid])).should == [  ].to_set
+  end
+  
   
 end
