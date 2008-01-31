e064a328caf34a0fe33fc36693ec167c0059d955
Yurii Rashkovskii <yrashk@issuesdone.com>
2008-01-31 18:41:14 +0200

    minor test/playing fix for StrokeObject

--- strokeobject-ruby/test/playing.rb
+++ strokeobject-ruby/test/playing.rb
@@ -9,10 +9,11 @@ require "strokeobject"
 @mem.add_chained_storage!(@fs)
 @mem.authoritative_source=@fs
 index_storage = StrokeDB::InvertedListFileStorage.new('test/storages/inverted_list_storage')
-# index_storage.clear!
 @index = StrokeDB::InvertedListIndex.new(index_storage)
 Stroke.default_store = StrokeDB::SkiplistStore.new(@mem,6, @index)
 @index.document_store = Stroke.default_store
+
+
 Stroke::Meta.new(:name => "User")
 unless u = @index.find(:__meta__ => User.document, :email => "yrashk@gmail.com").first
   puts "User not found, creating new user"
