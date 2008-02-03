102b4f77b3e352e7033bb8ceb3b0e71b86402c3e
Yurii Rashkovskii <yrashk@issuesdone.com>
2008-02-03 12:44:16 +0200

    Minor cleanup

--- strokeobject-ruby/strokeobject.rb
+++ strokeobject-ruby/strokeobject.rb
@@ -4,10 +4,6 @@ module Stroke
   VERSION = '0.0.1' + (RUBY_PLATFORM =~ /java/ ? '-java' : '')
   NIL_UUID = "00000000-0000-0000-0000-000000000000" # so called Nil UUID, should be used as special UUID for Meta meta
 
-  #
-  # Stroke.default_store accessor (should be thread-safe)
-  # Stroke.default_config accessor (should be thread-safe)
-  #
   class <<self
     def default_store
       StrokeDB.default_config.stores[:default]
