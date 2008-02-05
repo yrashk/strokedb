0623d32245c8f5d4554bb6bba52d758dec48c0af
Yurii Rashkovskii <yrashk@issuesdone.com>
2008-02-05 13:38:40 +0200

    MRI version uses uuidtools gem for random UUID generation (though it adds some performance overhead)

--- strokedb-ruby/lib/util/util.rb
+++ strokedb-ruby/lib/util/util.rb
@@ -2,7 +2,7 @@ require 'digest/sha2'
 
 module StrokeDB  
   module Util
-    
+
     module ::Enumerable
       def map_with_index
         collected=[]
@@ -11,7 +11,7 @@ module StrokeDB
       end
       alias :collect_with_index :map_with_index
     end
-    
+
     class HashWithSortedKeys < Hash
       def keys_with_sort
         keys_without_sort.sort
@@ -22,17 +22,13 @@ module StrokeDB
     def self.sha(str)
       Digest::SHA256.hexdigest(str)
     end
-    
-    # Stupid implementation.
-    # TODO: use gem install uuid (http://trac.labnotes.org/cgi-bin/trac.cgi/browser/uuid)
-    # valid one: "550e8400-e29b-41d4-a716-446655440000"
-    def self.random_uuid
-      a = "1234567890abcdef"
-      s = ''
-      [8,4,4,4,12].map do |len| 
-        (1..len).map{ a[rand(16),1] }.join
-      end.join '-'
+
+    unless RUBY_PLATFORM =~ /java/
+      require 'uuidtools'
+      def self.random_uuid
+        ::UUID.random_create.to_s
+      end
     end
-    
+
   end
 end
