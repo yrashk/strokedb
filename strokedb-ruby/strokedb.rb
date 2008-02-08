df6a74a4bbcec5aed558c31e5e0bbd5b0eb37581
Yurii Rashkovskii <yrashk@issuesdone.com>
2008-02-08 10:49:38 +0200

    Excluded all specs from rcov; strokedb loader is separated from few definitions it had below

--- strokedb-ruby/strokedb.rb
+++ strokedb-ruby/strokedb.rb
@@ -49,18 +49,3 @@ end
 
 SmartassLoader.new("lib/**/*").require!
 
-module StrokeDB
-  VERSION = '0.0.1' + (RUBY_PLATFORM =~ /java/ ? '-java' : '')
-  UUID_RE = /([a-f0-9]{8}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{12})/
-  VERSION_RE = /([a-f0-9]{64})/
-  NIL_UUID = "00000000-0000-0000-0000-000000000000" # so called Nil UUID, should be used as special UUID for Meta meta
-  
-  class <<self
-    def default_store
-      StrokeDB.default_config.stores[:default]
-    end
-  end
-  
-  class NoDefaultStoreError < Exception ; end
-  
-end
