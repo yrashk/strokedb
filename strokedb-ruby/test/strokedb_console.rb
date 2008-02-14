f224c2fbf7789568a5d6a881d3dd103e4a41ca43
Yurii Rashkovskii <yrashk@issuesdone.com>
2008-02-15 00:24:34 +0200

    test/console got save!, clear! and find

--- strokedb-ruby/test/strokedb_console.rb
+++ strokedb-ruby/test/strokedb_console.rb
@@ -1,4 +1,25 @@
 require "strokedb"
 include StrokeDB
-StrokeDB::Config.build :default => true, :base_path => 'test/storages/console'
+
 Doc = Document
+
+def save!
+  StrokeDB.default_store.chunk_storage.sync_chained_storages!
+  true
+end
+
+def build_config
+  StrokeDB::Config.build :default => true, :base_path => (File.dirname(__FILE__) + '/../test/storages/console')
+  true
+end
+
+def clear!
+  FileUtils.rm_rf File.dirname(__FILE__) + '/../test/storages/console'
+  build_config
+end
+
+def find(*args)
+  StrokeDB.default_store.find(*args)
+end
+
+build_config
