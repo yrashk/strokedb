18642fb716b09f277476d6bcf02bf346c11891f2
Yurii Rashkovskii <yrashk@issuesdone.com>
2008-02-15 11:19:32 +0200

    Help for test/console has been added

--- strokedb-ruby/test/strokedb_console.rb
+++ strokedb-ruby/test/strokedb_console.rb
@@ -22,4 +22,21 @@ def find(*args)
   StrokeDB.default_store.find(*args)
 end
 
+def h(*args)
+  puts %{
+    Commands:
+    
+    clear!      -- Clear the database (will erase all data in console's store)
+    save!       -- Save database (if you will quit without it, your changes will not be recorded)
+    find <uuid> -- Find document by UUID (example: find "a4430ff1-6cb4-4428-a292-7ab8b77de467")
+    
+    Aliases:
+    
+    Doc         -- StrokeDB::Document
+  }
+end
+
 build_config
+
+puts "StrokeDB #{StrokeDB::VERSION} Console"
+puts "Type 'h' for help"
