70df924e92c40765f972e30df82c20e5e13b2ff3
Oleg Andreev <oleganza@gmail.com>
2008-02-17 05:56:48 +0300

    fixed string stroke_diff

--- strokedb-ruby/lib/sync/stroke_diff.rb
+++ strokedb-ruby/lib/sync/stroke_diff.rb
@@ -3,7 +3,8 @@ require 'diff/lcs'
 
 module StrokeDB
   PATCH_REPLACE      = 'R'.freeze
-  PATCH_STRING       = 'S'.freeze
+  PATCH_STRING_PLUS  = '+'.freeze
+  PATCH_STRING_MINUS = '-'.freeze
   
   class ::Object
     def stroke_diff(to)
@@ -17,62 +18,90 @@ module StrokeDB
     def stroke_diff(to)
       return super(to) unless String === to
       return nil if self == to
-
+      #puts "Diffing strings: #{self.inspect} vs. #{to.inspect}"
       # lcs_diff     -> [ [Change, Change, ...], ... ]
       lcs_diff = Diff::LCS.diff(self, to)
+      # LCS.diff gives a huge list of of single-letter diffs.
+      # Here we union sequentional updates in a single updates.
+      # E.g. [['-', i, e], ... ['-', i + N, e]] => ['-', index, N]
+      #p :lcs_diff => lcs_diff.map{|a|a.map{|b|b.to_a}} 
       patchset = lcs_diff.map do |changes| 
-        # 1. Deletion
-        d_i1 = nil
-        d_i2 = nil
-        change = nil
-        
-        # BUG HERE!
-        chs = changes.dup
-        while change = chs.shift
-          if change.action == '-' && change.element != ''  # it can be ["-", pos, ""] (empty string)
-            d_i1 ||= change.position
-            d_i2 = change.position
-          end
-        end
-
-        # put shifted item back if it is '+'
-        changes.unshift(change) if change && change.action == '+'
-
-        # 2. Insertion
-        i_i1 = nil
-        new_sequence = nil
-        unless changes.empty?
-          new_sequence = changes.inject(""){|r,c| r << c.element; r }
-          i_i1 = changes[0].position
-        end
-
-        # 3. Determine kind of change
-        if i_i1
-          if d_i1
-            [PATCH_STRING+'r', d_i1, d_i2 - d_i1 + 1, new_sequence]
+        parts = []
+        last_part = changes.inject(nil) do |part, change|
+          if part && part[0] == change.action && part[3] == change.position - 1
+            part[3] += 1
+            part[2] << change.element
+            part
           else
-            [PATCH_STRING+'i', i_i1, 0,               new_sequence]
-          end
-        else
-          if d_i1
-            [PATCH_STRING+'d', d_i1, d_i2 - d_i1 + 1, ""]
-          else
-            raise "Diffed equal strings, oh my!"
-            nil # no difference
+            parts << part if part
+            # emit
+            [change.action, change.position, change.element, change.position]
           end
         end
-      end.compact
+        parts << last_part if last_part
+        parts.empty? ? nil : parts
+      end.compact.inject([]) do |patches, ps|
+        ps.map do |p|
+          patches << if p[0] == '+'
+            [PATCH_STRING_PLUS,  p[1], p[2]]
+          else
+            [PATCH_STRING_MINUS, p[1], p[2].size]
+          end
+        end
+        patches
+      end
+      #p patchset
       patchset.empty? ? nil : patchset
     end
     def stroke_patch(patch)
       return self unless patch
-      offset = 0
-      patch.inject(self.dup) do |to, change|
-        t, i, len, s = change
-        to[i + offset, len] = s
-        offset += s.size - len
-        to
+      #puts "#{self.inspect}.stroke_patch(#{patch.inspect}) "
+      res = ""
+      ai = bj = 0
+      patch.each do |change|
+        action, position, element = change
+        case action
+        when PATCH_STRING_MINUS
+          d = position - ai
+          if d > 0
+            res << self[ai, d]
+            ai += d
+            bj += d
+          end
+          ai += element # element == length
+          # while ai < position
+          #   res << self[ai, 1]
+          #   ai += 1
+          #   bj += 1
+          # end
+          # ai += 1
+        when PATCH_STRING_PLUS
+          d = position - bj
+          if d > 0
+            res << self[ai, d]
+            ai += d
+            bj += d
+          end
+          bj += element.size
+          # while bj < position
+          #   res << self[ai, 1]
+          #   ai += 1
+          #   bj += 1
+          # end
+          # bj += 1
+          res << element
+        end
       end
+      d = self.size - ai
+      res << self[ai, d] if d > 0
+
+      # while ai < src.size
+      #   res << self[ai, 1]
+      #   ai += 1
+      #   bj += 1
+      # end
+
+      res
     end
   end
   
@@ -115,10 +144,10 @@ end
 
 if __FILE__ == $0
   
-  letters = %w(a b c d)
+  letters = %w(a b c)
   def gen_str(letters)
     str = ""
-    len = rand(letters.size + 1)
+    len = rand(letters.size*2)
     len.times {
       str << letters[rand(letters.size)]
     }
@@ -139,6 +168,7 @@ if __FILE__ == $0
     rescue => e
       puts "Exception in the stroke_diff/patch!"
       puts e
+      puts e.backtrace.join("\n")
       p from
       p to
       puts "-"*40
