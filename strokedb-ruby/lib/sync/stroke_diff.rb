9332cbe0466b17feb1baaf96b5431f73051f9b5c
Oleg Andreev <oleganza@gmail.com>
2008-02-17 08:31:09 +0300

    added Hash#stroke_diff and stroke_patch

--- strokedb-ruby/lib/sync/stroke_diff.rb
+++ strokedb-ruby/lib/sync/stroke_diff.rb
@@ -179,14 +179,37 @@ module StrokeDB
 
   class ::Hash
     def stroke_diff(to)
-      return super(to) unless Array === to
+      return super(to) unless Hash === to
       return nil if self == to
       
+      all_keys = self.keys | to.keys
       
+      deleted_slots  = []
+      inserted_slots = {}
+      diffed_slots   = {}
+      
+      all_keys.each do |k|
+        unless to[k]
+          deleted_slots << k
+        else
+          unless self[k]
+            inserted_slots[k] = to[k] 
+          else
+            diff = self[k].stroke_diff(to[k]) 
+            diffed_slots[k] = diff if diff
+          end
+        end
+      end
+      [deleted_slots, inserted_slots, diffed_slots]
     end
     
     def stroke_patch(patch)
-      
+      res = self.dup
+      deleted_slots, inserted_slots, diffed_slots = patch
+      deleted_slots.each {|k| res.delete(k) }
+      res.merge!(inserted_slots)
+      diffed_slots.each {|k,v| res[k] = self[k].stroke_patch(v) }
+      res
     end
   end
 end
@@ -204,6 +227,16 @@ if __FILE__ == $0
     }
     str
   end
+  
+  from = {:_ => 0, :a => 1, :b => Object.new, :c => %w(1 2 3)}
+  to   = {:a => 1, :b => Object.new, :c => %w(1 2)}
+  
+  p from
+  p to
+  p from.stroke_diff(to)
+  p from.stroke_patch(from.stroke_diff(to))
+  
+  exit
   
   32.times {
     from = gen_str(letters).split(//u)
