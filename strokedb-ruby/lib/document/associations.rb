50a11970d2b4a84022b7c2468f0c0f88598c2798
Yurii Rashkovskii <yrashk@issuesdone.com>
2008-02-23 15:22:28 -0500

    Temporary workaround for has_many: we do not serialize has_many condition to meta

--- strokedb-ruby/lib/document/associations.rb
+++ strokedb-ruby/lib/document/associations.rb
@@ -2,12 +2,12 @@ module StrokeDB
 
   module Associations
     def has_many(slotname,reference_slotname,query={})
-      @has_many ||= {}
-      @has_many[slotname.to_s] = [reference_slotname.to_s,query]
-      @args.last.reverse_merge!(:has_many => @has_many)
-      when_slot_not_found do |doc, slotname|
-        ref_slotname, query = doc.meta[:has_many][slotname]
-        doc.store.index_store.find(query).select {|d| d.send(ref_slotname) == doc } 
+      # @has_many ||= {}
+      # @has_many[slotname.to_s] = [reference_slotname.to_s,query]
+      # @args.last.reverse_merge!(:has_many => @has_many)
+      when_slot_not_found do |doc, missed_slotname|
+        # ref_slotname, query = doc.meta[:has_many][slotname]
+        doc.store.index_store.find(query).select {|d| d.send(reference_slotname) == doc }  if slotname.to_s == missed_slotname.to_s 
       end
     end 
   end  
