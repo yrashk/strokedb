bd2599d2bf5ee8aef12db2f5e2fb02352f10790d
Yurii Rashkovskii <yrashk@issuesdone.com>
2008-02-29 00:07:37 +0200

    Experimental has_many :through support

--- strokedb-ruby/lib/document/associations.rb
+++ strokedb-ruby/lib/document/associations.rb
@@ -4,13 +4,21 @@ module StrokeDB
     def has_many(slotname,opts={})
       opts = opts.stringify_keys
       reference_slotname = opts['foreign_reference']
-      meta = opts['with_meta'] || slotname.to_s.singularize
-      meta = meta.camelize
+      
+      through = opts['through'] || []
+      through = [through] unless through.is_a?(Array)
+      
+      meta = (through.shift || slotname).to_s.singularize.camelize
+      
       query = opts['conditions'] || {}
       when_slot_not_found do |doc, missed_slotname|
         effective_query = query.merge(:__meta__ => meta.constantize.document)
         effective_reference_slotname = reference_slotname || doc.meta.name.tableize.singularize
-        doc.store.index_store.find(effective_query).select {|d| d.send(effective_reference_slotname) == doc }  if slotname.to_s == missed_slotname.to_s 
+        if slotname.to_s == missed_slotname.to_s 
+          doc.store.index_store.find(effective_query).select do |d| 
+            d[effective_reference_slotname] && d.send(effective_reference_slotname) == doc 
+          end.map {|d| through.each {|t| d = d.send(t)  } ; d}
+        end
       end
     end 
   end  
