8cadb16c58af8afffd3261d0b54d998d18ebcc1c
Oleg Andreev <oleganza@gmail.com>
2008-02-29 23:16:40 +0300

    bug found in a test/distributed_versioning

--- strokedb-ruby/test/distributed_versioning.rb
+++ strokedb-ruby/test/distributed_versioning.rb
@@ -40,11 +40,11 @@ episode4 = Movie.create!(:owner => yrashk, :title => "Episode 4")
 season1 = Playlist.create!(:title => "Californication. Season 1.", :items => [])
 season1.items = [ episode1, episode2 ]
 season1.save!
-version = season1.__version__
-puts "2-element version: #{version}"
+version = season1.__version__.dup
+puts "2-element version: #{version[12,4]}"
 season1.items << episode3
 season1.save!
-puts "3-element version: #{season1.__version__}"
+puts "3-element version: #{season1.__version__[12,4]}"
 
 oleg.playlists << season1
 oleg.save!
@@ -54,8 +54,19 @@ oleg.save!
 # In real app we fetch a copy via find anyway, 
 # so there's no need in a special Document#clone
 season1_clone = find(season1.uuid, version)
+pp season1_clone.__version__[12,4]
+pp season1_clone.__previous_version__[12,4]
+
 season1_clone.items << episode4
 season1_clone.save!
+
+puts "Oleg's and Yurii's playlist versions:"
+pp season1.__version__[12,4]
+pp season1_clone.__version__[12,4]
+puts "Oleg's and Yurii's playlist previous versions:"
+pp season1.__previous_version__[12,4]
+pp season1_clone.__previous_version__[12,4]
+
 
 yrashk.playlists << season1_clone.save!
 yrashk.save!
@@ -67,6 +78,16 @@ yrashk.playlists[0].save!  # TODO: Maybe, combine that in a one operation
 yrashk.save!               # (on_save callback in meta?)
 
 # TODO: inspect a tree of Playlist versions
+
+class VNode
+  attr_accessor :doc_version, :children
+end
+
+all_nodes_by_version = { }
+
+puts "Oleg's and Yurii's playlist versions:"
+pp oleg.playlists.first.__versions__.all.map{|v|v[12,4]}
+pp yrashk.playlists.first.__versions__.all.map{|v|v[12,4]}
 
 # TODO: merged_playlist = oleg.playlists.first.stroke_merge(yrashk.playlists.first)
 
