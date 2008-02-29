9899019d4a360b5e7982a3712bae63a08ec85b0d
Yurii Rashkovskii <yrashk@issuesdone.com>
2008-02-29 07:28:28 +0200

    Sync regression test puts test storage to a correct directory now

--- strokedb-ruby/spec/regression/sync.rb
+++ strokedb-ruby/spec/regression/sync.rb
@@ -3,7 +3,7 @@ require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 describe "Database sync" do
   
   before(:all) do
-    @path = File.dirname(__FILE__) + '/../test/storages/regression/database-sync'
+    @path = File.dirname(__FILE__) + '/../../test/storages/regression/database-sync'
     build_config!
     FileUtils.rm_rf @path
   end
