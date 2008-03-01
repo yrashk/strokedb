3403ba4c7c00fb773b5015291ac9465c6a4f9890
Oleg Andreev <oleganza@gmail.com>
2008-03-01 16:01:46 +0300

    moved stroke_diff spec helpers to a spec/sync/stroke_diff/spec_helper.rb

--- strokedb-ruby/spec/spec_helper.rb
+++ strokedb-ruby/spec/spec_helper.rb
@@ -28,28 +28,3 @@ def setup_index(store=nil)
   store.index_store = @index
   @index
 end
-
-def should_merge(base, a, b, r)
-  c, r1, r2 = base.stroke_merge(base.stroke_diff(a), base.stroke_diff(b))
-  c.should be_false
-  r1.should == r
-  r2.should == r
-  # another order
-  c, r1, r2 = base.stroke_merge(base.stroke_diff(b), base.stroke_diff(a))
-  c.should be_false
-  r1.should == r
-  r2.should == r
-end
-
-def should_yield_conflict(base, a, b, ra, rb)
-  c, r1, r2 = base.stroke_merge(base.stroke_diff(a), base.stroke_diff(b))
-  c.should be_true
-  r1.should == ra
-  r2.should == rb
-  # another order
-  c, r1, r2 = base.stroke_merge(base.stroke_diff(b), base.stroke_diff(a))
-  c.should be_true
-  r1.should == rb
-  r2.should == ra
-end
-
