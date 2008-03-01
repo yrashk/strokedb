45f82416317d69cdace99f75e0fb6b0a7726673c
Yurii Rashkovskii <yrashk@issuesdone.com>
2008-03-01 11:39:03 +0200

    Console got 'commented xmp' prompt mode

--- strokedb-ruby/test/environment.rb
+++ strokedb-ruby/test/environment.rb
@@ -1,3 +1,11 @@
+IRB.conf[:PROMPT][:CXMP] = { # name of prompt mode
+  :PROMPT_C=>nil,
+  :PROMPT_I=>nil, :PROMPT_N=>nil, :PROMPT_S=>nil,
+  :RETURN => "    #==>%s\n"        # format to return value
+}
+IRB.conf[:PROMPT_MODE] = :CXMP
+
+
 require 'pp'
 require "strokedb"
 include StrokeDB
