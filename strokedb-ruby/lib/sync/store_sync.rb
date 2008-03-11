d71160b2711b721eabd136d77acce01638018f71
Yurii Rashkovskii <yrashk@issuesdone.com>
2008-03-11 19:17:44 +0200

    StrokeDB::SynchronizationReport and SynchronizationConflict metas were added; Store#sync! has been updated accordingly

--- strokedb-ruby/lib/sync/store_sync.rb
+++ strokedb-ruby/lib/sync/store_sync.rb
@@ -11,9 +11,21 @@ module StrokeDB
       @uuid, @rev1, @rev2 = uuid, rev1, rev2
     end
   end
+
+  SynchronizationReport = Meta.new do
+    has_many :conflicts, :through => :synchronization_conflicts
+    on_new_document do |report|
+      report.added_documents = []
+      report.fast_forwarded_documents = []
+      report.non_matching_documents = []
+    end
+  end
+  
+  SynchronizationConflict = Meta.new
   
   class Store
     def sync!(docs,_timestamp=nil)
+      report = SynchronizationReport.new(:timestamp => timestamp.counter)
       existing_chain = {}
       docs.group_by {|doc| doc.uuid}.each_pair do |uuid, versions|
         doc = find(uuid)
@@ -30,26 +42,34 @@ module StrokeDB
       docs.group_by {|doc| doc.uuid}.each_pair do |uuid, versions|
         incoming_chain = find(uuid,versions.last.__version__).__versions__.all_versions
         if existing_chain[uuid].nil? or existing_chain[uuid].empty? # It is a new document
-          save_as_head!(find(uuid,versions.last.__version__))
+          added_doc = find(uuid,versions.last.__version__)
+          save_as_head!(added_doc)
+          report.added_documents << added_doc
         else
           begin
             sync = sync_chains(incoming_chain.reverse,existing_chain[uuid].reverse)
           rescue NonMatchingChains
-            raise NonMatchingDocumentCondition.new(uuid) # that will definitely leave garbage in the store (FIXME?)
+            # raise NonMatchingDocumentCondition.new(uuid) # that will definitely leave garbage in the store (FIXME?)
+            non_matching_doc = find(uuid)
+            report.non_matching_documents << non_matching_doc
+            next
           end
           resolution = sync.is_a?(Array) ? sync.first : sync
           case resolution
           when :up_to_date
             # nothing to do
           when :merge
-            raise ConflictCondition.new(uuid, sync[1], sync[2])
+            SynchronizationConflict.create!(:synchronization_report => report, :rev1 => sync[1], :rev2 => sync[2])
           when :fast_forward
-            save_as_head!(find(uuid,sync[1].last))
+            fast_forwarded_doc = find(uuid,sync[1].last)
+            save_as_head!(fast_forwarded_doc)
+            report.fast_forwarded_documents << fast_forwarded_doc
           else
             raise "Invalid sync resolution #{resolution}"
           end
         end
       end
+      report.save!
     end
     private
     
--- strokedb-ruby/spec/sync/store_sync_spec.rb
+++ strokedb-ruby/spec/sync/store_sync_spec.rb
@@ -3,55 +3,82 @@ require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 [SkiplistStore].each do |store|
   describe "#{store} that.sync!s documents in" do
     
-    before(:all) do
-      StrokeDB::Config.build :default => true, :store => :skiplist
+    before(:each) do
+      StrokeDB::Config.build :default => true, :store => :skiplist, :base_path => File.dirname(__FILE__) + '/../../test/storages/store_sync'
       @store = StrokeDB.default_store
-      another_cfg = StrokeDB::Config.build
+      another_cfg = StrokeDB::Config.build :base_path => File.dirname(__FILE__) + '/../../test/storages/store_sync_another'
       @another_store = another_cfg.stores[:default]
+    end
+    
+    after(:each) do
+      FileUtils.rm_rf File.dirname(__FILE__) + '/../../test/storages/store_sync'
+      FileUtils.rm_rf File.dirname(__FILE__) + '/../../test/storages/store_another'
     end
     
     it "should add document that does not yet exist" do
       doc = Document.create!(@another_store, :hello => 'world')
       doc.test = 'passed'
       doc.save!
-      @store.sync!(doc.__versions__.all.reverse)
+      sync_rep = @store.sync!(doc.__versions__.all.reverse)
       @store.find(doc.uuid,doc.__previous_version__).should == doc.__versions__.previous
       @store.find(doc.uuid,doc.__version__).should == doc
       @store.find(doc.uuid).should == doc
       @store.find(doc.uuid).__versions__.all.should_not include(nil)
+      sync_rep.conflicts.should be_empty
+      sync_rep.fast_forwarded_documents.should be_empty
+      sync_rep.added_documents.should == [doc]
     end
     
     it "should fast-forward document if applicable" do
       doc = Document.create!(@another_store, :hello => 'world')
       doc.test = 'passed'
       doc.save!
-      @store.sync!(doc.__versions__.all.reverse)
+      sync_rep = @store.sync!(doc.__versions__.all.reverse)
       doc_at_store = @store.find(doc.uuid)
+      sync_rep.conflicts.should be_empty
+      sync_rep.added_documents.should == [doc_at_store]
+      sync_rep.fast_forwarded_documents.should be_empty
       doc_at_store.ok = true
       doc_at_store.save!
-      @another_store.sync!(doc_at_store.__versions__.all.reverse)
-      @another_store.find(doc_at_store.uuid).should == doc_at_store
+      another_sync_rep = @another_store.sync!(doc_at_store.__versions__.all.reverse)
+      doc = @another_store.find(doc_at_store.uuid)
+      doc.should == doc_at_store
+      another_sync_rep.conflicts.should be_empty
+      another_sync_rep.added_documents.should be_empty
+      another_sync_rep.fast_forwarded_documents.should == [doc]
     end
     
-    it "should raise NonMatchingDocumentCondition if applicable" do
+    it "should create non-matching report if applicable" do
       doc = Document.create!(@another_store, :hello => 'world')
       doc.test = 'passed'
       doc.save!
       adoc = Document.create!(:uuid => doc.uuid)
-      lambda { @store.sync!(doc.__versions__.all.reverse) }.should raise_error(NonMatchingDocumentCondition)
+      sync_rep = @store.sync!(doc.__versions__.all.reverse)
+      sync_rep.conflicts.should be_empty
+      sync_rep.non_matching_documents.should == [adoc]
+      sync_rep.added_documents.should be_empty
+      sync_rep.fast_forwarded_documents.should be_empty
     end
     
     it "should do nothing if everything is up-to-date" do
       doc = Document.create!(@another_store, :hello => 'world')
       doc.test = 'passed'
       doc.save!
-      @store.sync!(doc.__versions__.all.reverse)
+      sync_rep = @store.sync!(doc.__versions__.all.reverse)
+      sync_rep.conflicts.should be_empty
+      sync_rep.non_matching_documents.should be_empty
+      sync_rep.added_documents.should_not be_empty
+      sync_rep.fast_forwarded_documents.should be_empty
       doc_at_store = @store.find(doc.uuid)
-      @another_store.sync!(doc_at_store.__versions__.all.reverse)
+      another_sync_rep = @another_store.sync!(doc_at_store.__versions__.all.reverse)
       @another_store.find(doc_at_store.uuid).should == doc_at_store
+      another_sync_rep.conflicts.should be_empty
+      another_sync_rep.non_matching_documents.should be_empty
+      another_sync_rep.added_documents.should be_empty
+      another_sync_rep.fast_forwarded_documents.should be_empty
     end
     
-    it "should raise ConflictCondition if applicable" do
+    it "should create SynchronizationConflict if applicable" do
       doc = Document.create!(@another_store, :hello => 'world')
       doc.test = 'passed'
       doc.save!
@@ -61,31 +88,26 @@ require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
       doc_at_store.save!
       doc.ok = false
       doc.save!
-      lambda { @another_store.sync!(doc_at_store.__versions__.all.reverse) }.should raise_error(ConflictCondition)
-      begin
-        @another_store.sync!(doc_at_store.__versions__.all.reverse)
-      rescue ConflictCondition
-        $!.rev1[1].should == doc_at_store.__version__
-        $!.rev2[1].should == doc.__version__
-      end
+      another_sync_rep = @another_store.sync!(doc_at_store.__versions__.all.reverse)
+      another_sync_rep.non_matching_documents.should be_empty
+      another_sync_rep.added_documents.should be_empty
+      another_sync_rep.fast_forwarded_documents.should be_empty
+      another_sync_rep.conflicts.should_not be_empty
+      conflict = another_sync_rep.conflicts.first
+      conflict.rev1[1].should == doc_at_store.__version__
+      conflict.rev2[1].should == doc.__version__
     end
     
     it "should update timestamp prior to.sync! if it is specified" do
       original_timestamp = @store.timestamp
       @store.sync!([],100)
-      @store.timestamp.counter.should == 100
+      @store.timestamp.counter.should >= 100
       @store.timestamp.should_not == original_timestamp
       original_timestamp = @store.timestamp
       @store.sync!([],LTS.new(200))
-      @store.timestamp.counter.should == 200
+      @store.timestamp.counter.should >= 200
       @store.timestamp.should_not == original_timestamp
     end
-    
-    it "should not update timestamp prior to.sync! if it is not specified" do
-      original_timestamp = @store.timestamp
-      @store.sync!([])
-      @store.timestamp.counter.should == original_timestamp.counter
-    end
-    
+        
   end
 end
