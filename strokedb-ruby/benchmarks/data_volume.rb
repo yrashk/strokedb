075587927ea94369c4ccf06d833c156a5f30f2a3
Yurii Rashkovskii <yrashk@issuesdone.com>
2008-03-19 21:08:17 +0200

    DataVolume has dropped Hindex support; compond types decomposition is optional now

--- strokedb-ruby/benchmarks/data_volume.rb
+++ strokedb-ruby/benchmarks/data_volume.rb
@@ -8,58 +8,60 @@ include Benchmark
 
 SimpleSkiplist.optimize!(:C)
 
-[2000].each do |n|
+[true,false].each do |decompose|
+  [2000].each do |n|
 
-  record = "D"*512
+    record = "D"*512
 
-  bm(60) do |x| 
-    FileUtils.rm_rf @path
-    data_volume = DataVolume.new(:path => @path)
+    bm(60) do |x| 
+      FileUtils.rm_rf @path
+      data_volume = DataVolume.new(:path => @path, :decompose_compound_types => decompose)
 
-    records = []
-    n.times {|v| records << {"static" => "unique", "some_val_#{v}" => v,"some_val1_#{v}" => "_#{v}", "some_valX_#{v}" => "#{v}_"   } }
-    
-    offsets = []
-    x.report("Inserting #{n} complex different records") do
-      records.each {|rec| offsets << data_volume.insert!(rec) }
-    end
+      records = []
+      n.times {|v| records << {"static" => "unique", "some_val_#{v}" => v,"some_val1_#{v}" => "_#{v}", "some_valX_#{v}" => "#{v}_"   } }
 
-    x.report("Reading #{n} complex different records") do
-      offsets.each do |offset| 
-        data_volume.read(offset)  
+      offsets = []
+      x.report("Inserting #{n} complex different records (decompose: #{decompose})") do
+        records.each {|rec| offsets << data_volume.insert!(rec) }
       end
+
+      x.report("Reading #{n} complex different records (decompose: #{decompose})") do
+        offsets.each do |offset| 
+          data_volume.read(offset)  
+        end
+      end
+
+      data_volume.close!
+
     end
-    
-    data_volume.close!
 
   end
 
-end
+  [2000].each do |n|
 
-[2000].each do |n|
+    record = "D"*512
 
-  record = "D"*512
+    bm(60) do |x| 
+      FileUtils.rm_rf @path
+      data_volume = DataVolume.new(:path => @path, :decompose_compound_types => decompose)
 
-  bm(60) do |x| 
-    FileUtils.rm_rf @path
-    data_volume = DataVolume.new(:path => @path)
+      records = []
+      n.times {|v| records << {"static" => "unique" } }
 
-    records = []
-    n.times {|v| records << {"static" => "unique" } }
-    
-    offsets = []
-    x.report("Inserting #{n} complex static records") do
-      records.each {|rec| offsets << data_volume.insert!(rec)  }
-    end
-
-    x.report("Reading #{n} complex static records") do
-      offsets.each do |offset| 
-        data_volume.read(offset)  
+      offsets = []
+      x.report("Inserting #{n} complex static records (decompose: #{decompose})") do
+        records.each {|rec| offsets << data_volume.insert!(rec)  }
       end
+
+      x.report("Reading #{n} complex static records (decompose: #{decompose})") do
+        offsets.each do |offset| 
+          data_volume.read(offset)  
+        end
+      end
+
+      data_volume.close!
+
     end
-    
-    data_volume.close!
 
   end
-
 end
--- strokedb-ruby/lib/volumes/data_volume.rb
+++ strokedb-ruby/lib/volumes/data_volume.rb
@@ -3,25 +3,18 @@ module StrokeDB
   class DataVolume
 
     attr_reader :hindex
-    
+
     def initialize(options = {})
       @options = options.stringify_keys
-      FileUtils.mkdir_p @options['path']
-      @hindex = Index::H.new(@options)
       initialize_file
     end
-    
+
     def insert!(record)
       serialized=serialize(record)
-      if position = @hindex.find(serialized)
-        return position
-      else
-        @file.seek(0, IO::SEEK_END)
-        position = @file.pos
-        @file.write([serialized.size].pack('N')+serialized)
-        @hindex.insert(serialized,position)
-        position
-      end
+      @file.seek(0, IO::SEEK_END)
+      position = @file.pos
+      @file.write([serialized.size].pack('N')+serialized)
+      position
     end
 
 
@@ -30,12 +23,8 @@ module StrokeDB
       deserialize(@file.readbytes(@file.readbytes(4).unpack('N').first))
     end
 
-    def index(record)
-      @hindex.find(serialize(record))
-    end
-
     def path
-      File.join(@options['path'],'datavol')
+      @options['path']
     end
 
     def close!
@@ -49,28 +38,32 @@ module StrokeDB
     end
 
     def serialize(record)
-      case record
-      when Array
-        record = record.map {|v| insert!(v) }
-      when Hash
-        new_record = {}
-        record.each_pair do |k,v|
-          new_record[insert!(k)] = insert!(v)
+      if @options['decompose_compound_types']
+        case record
+        when Array
+          record = record.map {|v| insert!(v) }
+        when Hash
+          new_record = {}
+          record.each_pair do |k,v|
+            new_record[insert!(k)] = insert!(v)
+          end
+          record = new_record
         end
-        record = new_record
       end
       StrokeDB::serialize(record)
     end
 
     def deserialize(record)
       value = StrokeDB::deserialize(record)
-      case value
-      when Array
-        LazyMappingArray.new(value).map_with {|v| read(v) unless v.nil?}.unmap_with{|v| index(v)}
-      when Hash
-        LazyMappingHash.new(value).map_with {|v| read(v) unless v.nil?}.unmap_with{|v| index(v)}
-      else
-        value
+      if @options['decompose_compound_types']
+        case value
+        when Array
+          LazyMappingArray.new(value).map_with {|v| read(v) unless v.nil?}#.unmap_with{|v| index(v)}
+        when Hash
+          LazyMappingHash.new(value).map_with {|v| read(v) unless v.nil?}#.unmap_with{|v| index(v)}
+        else
+          value
+        end
       end
     end
 
