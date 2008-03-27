130a7c163899f7df279338501e7946bc0e2b6301
Yurii Rashkovskii <yrashk@issuesdone.com>
2008-03-27 02:58:03 +0200

    ChainedStorage spec has been moved

--- strokedb-ruby/spec/stores/chained_storages_spec.rb
+++ strokedb-ruby/spec/stores/chained_storages_spec.rb
@@ -1,4 +1,4 @@
-require File.expand_path(File.dirname(__FILE__) + '/../../spec_helper')
+require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 
 Storage.subclasses.map{|e| e.constantize}.each do |storage|
   describe "Chained chunk storage (#{storage})" do
