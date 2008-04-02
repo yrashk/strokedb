24ad5a1684c45eb20d7f61402e1f0f80f0018715
oleg dashevskii <be9@be9.ru>
2008-04-02 13:23:39 +0700

    Added some docs for validating methods.

--- strokedb-ruby/lib/document/validations.rb
+++ strokedb-ruby/lib/document/validations.rb
@@ -1,3 +1,19 @@
+# TODO (taken from ActiveRecord):
+#   validate (with a method)
+#   validates_each
+#   validates_confirmation_of
+#   validates_acceptance_of
+#   validates_length_of
+#   validates_uniqueness_of
+#   validates_format_of
+#   validates_inclusion_of
+#   validates_exclusion_of
+#   validates_associated
+#   validates_numericality_of
+#
+#   :if and :unless options for all
+#
+# Consider also using validatable gem (DataMapper is now switching to it)
 module StrokeDB
   module Validations
     class ValidationError < StandardError
@@ -12,6 +28,23 @@ module StrokeDB
       end
     end
     
+    # Validates that the specified slot exists in the document. Happens by default on save. Example:
+    #
+    #   Person = Meta.new do
+    #     validates_presence_of :first_name
+    #   end
+    #
+    # The first_name slot must be in the document.
+    #
+    # Configuration options:
+    # * <tt>message</tt> - A custom error message (default is: "should be present on ...")
+    # * <tt>on</tt> - Specifies when this validation is active (default is :save, other options :create, :update)
+    # * <tt>if</tt> - (UNSUPPORTED) Specifies a method, proc or string to call to determine if the validation should
+    #   occur (e.g. :if => :allow_validation, or :if => Proc.new { |user| user.signup_step > 2 }).  The
+    #   method, proc or string should return or evaluate to a true or false value.
+    # * <tt>unless</tt> - (UNSUPPORTED) Specifies a method, proc or string to call to determine if the validation should
+    #   not occur (e.g. :unless => :skip_validation, or :unless => Proc.new { |user| user.signup_step <= 2 }).  The
+    #   method, proc or string should return or evaluate to a true or false value.
     def validates_presence_of(slotname, opts={}, &block)
       opts = opts.stringify_keys
       slotname = slotname.to_s
@@ -23,14 +56,34 @@ module StrokeDB
       end
     end 
     
-    def validates_type_of(slotname,opts={},&block)
+    # Validates that the specified slot value has a specific type. Happens by default on save. Example:
+    #
+    #   Person = Meta.new do
+    #     validates_type_of :first_name, :as => :string
+    #   end
+    #
+    # The first_name slot must be of type String (is_a? String).
+    #
+    # Configuration options:
+    # * <tt>message</tt> - A custom error message (default is: "should be of type ...")
+    # * <tt>on</tt> - Specifies when this validation is active (default is :save, other options :create, :update)
+    # * <tt>if</tt> - (UNSUPPORTED) Specifies a method, proc or string to call to determine if the validation should
+    #   occur (e.g. :if => :allow_validation, or :if => Proc.new { |user| user.signup_step > 2 }).  The
+    #   method, proc or string should return or evaluate to a true or false value.
+    # * <tt>unless</tt> - (UNSUPPORTED) Specifies a method, proc or string to call to determine if the validation should
+    #   not occur (e.g. :unless => :skip_validation, or :unless => Proc.new { |user| user.signup_step <= 2 }).  The
+    #   method, proc or string should return or evaluate to a true or false value.
+    # 
+    # === Warning
+    # When the slot doesn't exist, validation gets skipped.
+    def validates_type_of(slotname, opts={}, &block)
       opts = opts.stringify_keys
       slotname = slotname.to_s
       on = (opts['on'] || 'save').to_s.downcase
       unless validation_type = opts['as']
-        raise "validates_type_of requires :as => type"
+        raise ArgumentError, "validates_type_of requires :as => type"
       end
-      message = opts['message'] || 'puts #{meta}\'s #{slotname} should be of type #{validation_type}'
+      message = opts['message'] || '#{meta}\'s #{slotname} should be of type #{validation_type}'
       
       @meta_initialization_procs << Proc.new do
         @args.last.reverse_merge!("validates_type_of_#{slotname}" => { :meta => name, :slotname => slotname, :message => message, :on => on, :type => validation_type })
