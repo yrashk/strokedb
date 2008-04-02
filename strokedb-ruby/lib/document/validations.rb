5c174e3747e1fb22a05617cbc7fe846b331efe7a
oleg dashevskii <be9@be9.ru>
2008-04-02 12:10:31 +0700

    DRY validations spec, refactor initialize_validations

--- strokedb-ruby/lib/document/validations.rb
+++ strokedb-ruby/lib/document/validations.rb
@@ -1,19 +1,19 @@
 module StrokeDB
-
   module Validations
 
     class ValidationError < StandardError
       attr_reader :document, :meta, :slotname, :on
+      
       def initialize(doc,meta,slotname,on,msg)
         @document, @meta, @slotname, @on, @msg = doc,meta,slotname,on,msg
       end
+      
       def message
         eval("\"#{@msg}\"")
       end
     end
     
-
-    def validates_presence_of(slotname,opts={},&block)
+    def validates_presence_of(slotname, opts={}, &block)
       opts = opts.stringify_keys
       slotname = slotname.to_s
       on = (opts['on'] || 'save').to_s.downcase
@@ -22,15 +22,14 @@ module StrokeDB
       @meta_initialization_procs << Proc.new do
         @args.last.reverse_merge!("validates_presence_of_#{slotname}" => { :meta => name, :slotname => slotname, :message => message, :on => on })
       end
-    end 
-    
+    end
+
     private 
 
     def initialize_validations
       before_save(:validates_presence_of) do |doc|
-        presence_validations = doc.meta.slotnames.grep(/^validates_presence_of_/).map{|v| v.gsub(/^validates_presence_of_(.+)/,'\\1')}
-        presence_validations.each do |slotname_to_validate|
-          if validation=doc.meta["validates_presence_of_#{slotname_to_validate}"] 
+        grep_validations(doc, "validates_presence_of_") do |slotname_to_validate, meta_slotname|
+          if validation = doc.meta[meta_slotname] 
             on = validation['on']
             if (on == 'create' && doc.new? && !doc.has_slot?(slotname_to_validate)) ||
                (on == 'update' && !doc.new? && !doc.has_slot?(slotname_to_validate)) ||
@@ -41,8 +40,15 @@ module StrokeDB
         end
       end
     end
-    
-  end  
 
+    def grep_validations(doc, prefix)
+      doc.meta.slotnames.each do |slotname|
+        if slotname[0..(prefix.length - 1)] == prefix
+          yield slotname[prefix.length..-1], slotname
+        end
+      end
+    end
+
+  end  
 
 end
