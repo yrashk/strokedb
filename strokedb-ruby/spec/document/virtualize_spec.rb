8a6d75d6b905e599159a539481fee1d1bf9a8a12
oleg dashevskii <be9@be9.ru>
2008-04-05 03:16:55 +0700

    Add :restore option on virtualizes and RDoc. Drop :if and :unless stuff for now

--- strokedb-ruby/spec/document/virtualize_spec.rb
+++ strokedb-ruby/spec/document/virtualize_spec.rb
@@ -3,7 +3,8 @@ require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 def setup
   setup_default_store
   setup_index
-  Object.send!(:remove_const, 'Foo') if defined?(Foo)
+  Object.send!(:remove_const, 'Foo') if defined? Foo
+  Object.send!(:remove_const, 'Bar') if defined? Bar
 end
 
 describe "Document slot virtualization" do
@@ -11,7 +12,7 @@ describe "Document slot virtualization" do
     setup
 
     Foo = Meta.new do 
-      virtualizes :virtual_slot
+      virtualizes [ :virtual_slot, :another_slot ]
     end
   end
 
@@ -21,10 +22,12 @@ describe "Document slot virtualization" do
   end
 
   it "should retain its value after save" do
-    f = Foo.new(:virtual_slot => 123)
+    f = Foo.new(:virtual_slot => 123, :another_slot => 456)
     f.virtual_slot.should == 123
+    f.another_slot.should == 456
     f.save!
     f.virtual_slot.should == 123
+    f.another_slot.should == 456
   end
 
   it "should not serialize slot value" do
@@ -59,9 +62,19 @@ describe "Document slot virtualization" do
     lambda { b.save! }.should_not raise_error(InvalidDocumentError)
   end
 
-  it "should respect :if"
-  it "should respect :unless"
-  it "should respect both :if and :unless"
+  it "shouldn't restore virtual slot with :restore set to false" do
+    Bar = Meta.new { virtualizes :mylovelyslot, :restore => false }
+    b = Bar.new(:mylovelyslot => "abc")
+    b.mylovelyslot.should == "abc"
+    b.save!
+    b.has_slot?(:mylovelyslot).should be_false
+  end
+  
+  it "should preserve version after saving document with :restore set to false" do
+    Bar = Meta.new { virtualizes :mylovelyslot, :restore => false }
+    b = Bar.new(:mylovelyslot => "abc")
+    lambda do
+      b.save!
+    end.should_not change(b,:version)
+  end
 end
-
-
