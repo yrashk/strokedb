f95959c2a372f701ed1dcd5d752ff2005defb76c
Yurii Rashkovskii <yrashk@idbns.com>
2008-04-17 16:10:45 +0300

    Much better way to load libuuid

--- strokedb-ruby/lib/util/uuid.rb
+++ strokedb-ruby/lib/util/uuid.rb
@@ -4,52 +4,26 @@ begin
     require 'rubygems'
     require 'inline'
 
-    def self.dlpath
-      dlpath = ['/opt/local/lib/libuuid.dylib',*(`locate libuuid.so`.split.concat(`locate libuuid.dylib`.split))].select{|f| File.exists?(f)}.uniq.first
-      raise NotImplementedError, "cannot find libuuid" unless dlpath
-      dlpath
-    end
-
+    C_FLAGS = `uuid-config --cflags`.chomp
+    LD_FLAGS = `uuid-config --ldflags --libs`.chomp
+    
     inline(:C) do |builder|
-      builder.add_compile_flags '-I /opt/local/include'
+      builder.add_compile_flags C_FLAGS
+      builder.add_link_flags LD_FLAGS
       builder.prefix %{
         #include "dlfcn.h"
         #include "ossp/uuid.h"
-
-        typedef uuid_rc_t (*uuid_create_t)(uuid_t**);
-        typedef uuid_rc_t (*uuid_make_t    )(uuid_t *, unsigned int);
-        typedef uuid_rc_t (*uuid_export_t  )(const uuid_t *, uuid_fmt_t, void *, size_t *);
-        typedef uuid_rc_t (*uuid_destroy_t )(uuid_t *);
-        typedef uuid_rc_t (*uuid_import_t  )(uuid_t *, uuid_fmt_t, const void *, size_t);
-
-        static uuid_create_t  uuid_create1;
-        static uuid_make_t    uuid_make1;
-        static uuid_export_t  uuid_export1;
-        static uuid_destroy_t uuid_destroy1;
-        static uuid_import_t  uuid_import1;
-      }
-      builder.add_to_init %{
-        VALUE i_dlpath = rb_intern("dlpath");
-        VALUE dlpath = rb_funcall(c, i_dlpath, 0);
-        void* handle = dlopen(StringValuePtr(dlpath), RTLD_NOW); 
-
-        uuid_create1  = dlsym(handle, "uuid_create");
-        uuid_make1    = dlsym(handle, "uuid_make");
-        uuid_export1  = dlsym(handle, "uuid_export");
-        uuid_destroy1 = dlsym(handle, "uuid_destroy");
-        uuid_import1  = dlsym(handle, "uuid_import");
-
       }
       builder.c %{
         VALUE random_uuid() 
         {
           uuid_t *uuid;
           char *str;
-          uuid_create1(&uuid);
-          uuid_make1(uuid, UUID_MAKE_V4);
+          uuid_create(&uuid);
+          uuid_make(uuid, UUID_MAKE_V4);
           str = NULL;
-          uuid_export1(uuid, UUID_FMT_STR, &str, NULL);
-          uuid_destroy1(uuid);
+          uuid_export(uuid, UUID_FMT_STR, &str, NULL);
+          uuid_destroy(uuid);
           VALUE r = rb_str_new(str,36);
           free(str);
           return r;
@@ -60,11 +34,11 @@ begin
         {
           uuid_t *uuid;
           char *str;
-          uuid_create1(&uuid);
-          uuid_make1(uuid, UUID_MAKE_V4);
+          uuid_create(&uuid);
+          uuid_make(uuid, UUID_MAKE_V4);
           str = NULL;
-          uuid_export1(uuid, UUID_FMT_BIN, &str, NULL);
-          uuid_destroy1(uuid);
+          uuid_export(uuid, UUID_FMT_BIN, &str, NULL);
+          uuid_destroy(uuid);
           VALUE r = rb_str_new(str,16);
           free(str);
           return r;
@@ -76,11 +50,11 @@ begin
         {
           uuid_t *uuid;
           char *str;
-          uuid_create1(&uuid);
-          uuid_import1(uuid, UUID_FMT_STR, STR2CSTR(r_uuid), 36);
+          uuid_create(&uuid);
+          uuid_import(uuid, UUID_FMT_STR, STR2CSTR(r_uuid), 36);
           str = NULL;
-          uuid_export1(uuid, UUID_FMT_BIN, &str, NULL);
-          uuid_destroy1(uuid);
+          uuid_export(uuid, UUID_FMT_BIN, &str, NULL);
+          uuid_destroy(uuid);
           VALUE r = rb_str_new(str,16);
           free(str);
           return r;
@@ -92,11 +66,11 @@ begin
         {
           uuid_t *uuid;
           char *str;
-          uuid_create1(&uuid);
-          uuid_import1(uuid, UUID_FMT_BIN, STR2CSTR(r_uuid), 36);
+          uuid_create(&uuid);
+          uuid_import(uuid, UUID_FMT_BIN, STR2CSTR(r_uuid), 36);
           str = NULL;
-          uuid_export1(uuid, UUID_FMT_STR, &str, NULL);
-          uuid_destroy1(uuid);
+          uuid_export(uuid, UUID_FMT_STR, &str, NULL);
+          uuid_destroy(uuid);
           VALUE r = rb_str_new(str,36);
           free(str);
           return r;
