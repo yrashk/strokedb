a0bc84f575a782b3167489cb2657b83759c56afc
Claudio Perez Gamayo <crossblaim@gmail.com>
2008-04-17 21:00:58 +0200

    add String#underscore method

--- lib/core_ext/string.rb
+++ lib/core_ext/string.rb
@@ -10,4 +10,12 @@ class String
   def /(o)
     File.join(self, o.to_s)
   end
+  
+  def underscore
+    self.to_s.gsub(/::/, '/').
+      gsub(/([A-Z]+)([A-Z][a-z])/,'\1_\2').
+      gsub(/([a-z\d])([A-Z])/,'\1_\2').
+      tr("-", "_").
+      downcase
+  end
 end
