a6d4c6e18edc3495f0ee24fe159d6177d62aab1b
Oleg Andreev <oleganza@idbns.com>
2008-04-22 04:36:45 +0400

    Meta#find_or_create calls block for existed document

--- lib/strokedb/document/meta.rb
+++ lib/strokedb/document/meta.rb
@@ -193,7 +193,7 @@ module StrokeDB
     #
     def find_or_create(*args, &block)
       result = find(*args)
-      result.empty? ? create!(*args, &block) : result.first
+      result.empty? ? create!(*args, &block) : (r = result.first; block_given? ? yield(r) : nil; r)
     end
 
     def inspect
