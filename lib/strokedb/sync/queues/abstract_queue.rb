80a0fed3c86583b836e2a6a77979949b124dfd62
Oleg Andreev <oleganza@idbns.com>
2008-04-25 01:46:59 +0400

    added comments to on_receive, pop in abstract_queue

--- lib/strokedb/sync/queues/abstract_queue.rb
+++ lib/strokedb/sync/queues/abstract_queue.rb
@@ -10,10 +10,14 @@ module StrokeDB
     end
     alias :<< :push
     
+    # on_receive callback is called when the queue decides do pop
+    # out an object. If there is no on_receive, it waits for the #pop call.
     def on_recieve(&block)
       @on_receive = block || @on_receive
     end
     
+    # Get an object out of the queue. 
+    # nil is returned when nothing is to be returned.
     def pop
       raise "Abstract queue cannot return objects"
     end
