8d3d5391632924a669c50f4664c763ccbf390ce8
Oleg Andreev <oleganza@idbns.com>
2008-04-28 01:30:46 +0400

    spec for view.find :key => ... added [WARNING! random bug detected: 20% cases -> wrong results order]

--- spec/lib/strokedb/views/view_spec.rb
+++ spec/lib/strokedb/views/view_spec.rb
@@ -27,11 +27,11 @@ end
 
 describe "'Has many comments' view" do
   
-  before(:each) do
+  before(:all) do
     setup_default_store
     @view = View.define(:name => "post_comments") do |view|
       def view.map(uuid, doc)
-        doc['type'] == "comment" ? [[[doc.parent, doc.created_at], doc]] : nil
+        doc['type'] =~ /comment/ ? [[[doc.parent, doc.created_at], doc]] : nil
       end
     end
     
@@ -39,18 +39,18 @@ describe "'Has many comments' view" do
     @article2 = Document.create! :type => "post"
     @article3 = Document.create! :type => "post"
     
-    @comment11 = Document.create! :type => "comment", :parent => @article1, :created_at => Time.now
-    @comment12 = Document.create! :type => "comment", :parent => @article1, :created_at => Time.now
-    @comment13 = Document.create! :type => "comment", :parent => @article1, :created_at => Time.now
+    @comment11 = Document.create! :type => "comment11", :parent => @article1, :created_at => Time.now+1
+    @comment12 = Document.create! :type => "comment12", :parent => @article1, :created_at => Time.now+2
+    @comment13 = Document.create! :type => "comment13", :parent => @article1, :created_at => Time.now+3
     
-    @comment21 = Document.create! :type => "comment", :parent => @article2, :created_at => Time.now
-    @comment22 = Document.create! :type => "comment", :parent => @article2, :created_at => Time.now
+    @comment21 = Document.create! :type => "comment21", :parent => @article2, :created_at => Time.now+4
+    @comment22 = Document.create! :type => "comment22", :parent => @article2, :created_at => Time.now+5
     
     # shuffled order to ensure, items are sorted correctly afterwards
     @view.update(@article3)
     @view.update(@comment22)
     @view.update(@comment21)
-    @view.update(@article2)    
+    @view.update(@article2)
     @view.update(@comment12)
     @view.update(@comment13)
     @view.update(@article1)
@@ -59,7 +59,15 @@ describe "'Has many comments' view" do
   
   it "should find all the comments sorted by date" do
     @view.find.should == [@comment11, @comment12, @comment13, @comment21, @comment22]
+    @view.find.should == [@comment11, @comment12, @comment13, @comment21, @comment22]
   end
+  
+  it "should find all the article's comments" do
+    @view.find(:key => @article1).should == [@comment11, @comment12, @comment13]
+    @view.find(:key => @article2).should == [@comment21, @comment22]
+    @view.find(:key => @article3).should == [ ]
+  end
+  
   
 end
 
