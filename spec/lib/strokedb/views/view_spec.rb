29cb7f91c65bf68f8334c717a149a411290cc5a8
Oleg Andreev <oleganza@idbns.com>
2008-04-28 01:36:51 +0400

    fixed random bug with random uuids sorting, added specs for :limit & :offset

--- spec/lib/strokedb/views/view_spec.rb
+++ spec/lib/strokedb/views/view_spec.rb
@@ -39,27 +39,29 @@ describe "'Has many comments' view" do
     @article2 = Document.create! :type => "post"
     @article3 = Document.create! :type => "post"
     
-    @comment11 = Document.create! :type => "comment11", :parent => @article1, :created_at => Time.now+1
-    @comment12 = Document.create! :type => "comment12", :parent => @article1, :created_at => Time.now+2
-    @comment13 = Document.create! :type => "comment13", :parent => @article1, :created_at => Time.now+3
+    @comment11 = Document.create! :type => "comment11", :parent => @article1, :created_at => Time.now
+    @comment12 = Document.create! :type => "comment12", :parent => @article1, :created_at => Time.now
+    @comment13 = Document.create! :type => "comment13", :parent => @article1, :created_at => Time.now
     
-    @comment21 = Document.create! :type => "comment21", :parent => @article2, :created_at => Time.now+4
-    @comment22 = Document.create! :type => "comment22", :parent => @article2, :created_at => Time.now+5
+    @comment21 = Document.create! :type => "comment21", :parent => @article2, :created_at => Time.now
+    @comment22 = Document.create! :type => "comment22", :parent => @article2, :created_at => Time.now
     
     # shuffled order to ensure, items are sorted correctly afterwards
     @view.update(@article3)
     @view.update(@comment22)
-    @view.update(@comment21)
+    @view.update(@comment11)
     @view.update(@article2)
     @view.update(@comment12)
     @view.update(@comment13)
     @view.update(@article1)
-    @view.update(@comment11)
+    @view.update(@comment21)
   end
   
   it "should find all the comments sorted by date" do
-    @view.find.should == [@comment11, @comment12, @comment13, @comment21, @comment22]
-    @view.find.should == [@comment11, @comment12, @comment13, @comment21, @comment22]
+    results = @view.find
+    # since article UUID can be anything 
+   (results == [@comment11, @comment12, @comment13,    @comment21, @comment22] || 
+    results == [@comment21, @comment22,    @comment11, @comment12, @comment13]).should == true
   end
   
   it "should find all the article's comments" do
@@ -68,6 +70,17 @@ describe "'Has many comments' view" do
     @view.find(:key => @article3).should == [ ]
   end
   
+  it "should find all the article's comments with limit" do
+    @view.find(:key => @article1, :limit => 2).should == [@comment11, @comment12]
+    @view.find(:key => @article2, :limit => 2).should == [@comment21, @comment22]
+    @view.find(:key => @article3, :limit => 2).should == [ ]
+  end
+
+  it "should find all the article's comments with offset and limit" do
+    @view.find(:key => @article1, :offset => 1, :limit => 2).should == [@comment12, @comment13]
+    @view.find(:key => @article2, :offset => 1, :limit => 2).should == [@comment22]
+    @view.find(:key => @article3, :offset => 1, :limit => 2).should == [ ]
+  end
   
 end
 
