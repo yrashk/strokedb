8c24572711a8b960f2b61f28e47412475b69e43d
Oleg Andreev <oleganza@idbns.com>
2008-04-30 09:57:51 +0400

    added meta_initialization proc

--- lib/strokedb/document/index_slots.rb
+++ lib/strokedb/document/index_slots.rb
@@ -8,17 +8,18 @@ module StrokeDB
     #
     def index_slots(*args)
       slots = args.map{|a| a.is_a?(Array) ? a.map{|e|e.to_s} : [a.to_s] }
-      view_name = "index_slots_for_#{name}:#{nsurl}"
-      doc = document # metadocument
-      opts = { :only => name, :slots => slots, :doc_meta => doc }
-      doc.index_slots_view = View.define!(view_name, opts) do |view|
-        def view.map(uuid, doc)
-          meta = self['doc_meta']
-          slots.each do |sl|
-            k = sl.inject([]) do |a, sn|
-              a << sn << doc[sn]
+      @meta_initialization_procs << Proc.new do
+        view_name = "index_slots_for_#{name}:#{nsurl}"
+        opts = { :only => name, :slots => slots, :doc_meta => doc }
+        doc.index_slots_view = View.define!(view_name, opts) do |view|
+          def view.map(uuid, doc)
+            meta = self['doc_meta']
+            slots.each do |sl|
+              k = sl.inject([]) do |a, sn|
+                a << sn << doc[sn]
+              end
+              [[meta, k], doc]
             end
-            [[meta, k], doc]
           end
         end
       end
