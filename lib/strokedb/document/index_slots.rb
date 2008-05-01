f543d0effa827695f36487c17024f8b46ac8e130
Oleg Andreev <oleganza@idbns.com>
2008-05-01 23:15:47 +0400

    added some comment to a view for index_slots.rb

--- lib/strokedb/document/index_slots.rb
+++ lib/strokedb/document/index_slots.rb
@@ -7,7 +7,10 @@ module StrokeDB
     #   end
     #
     def index_slots(*args)
+      # Wrap all slots into an array:
+      # [:a, :b, [:x, :y]]  => [ [:a], [:b], [:x, :y] ]
       slots = args.map{|a| a.is_a?(Array) ? a.map{|e|e.to_s} : [a.to_s] }
+      
       @meta_initialization_procs << Proc.new do
         view_name = "index_slots_for_#{name}:#{nsurl}"
         opts = { :only => name, :slots => slots, :doc_meta => doc }
@@ -15,7 +18,9 @@ module StrokeDB
           def view.map(uuid, doc)
             meta = self['doc_meta']
             slots.each do |sl|
+              # construct a key out of the list of slots
               k = sl.inject([]) do |a, sn|
+                # push [slot_name, slot_value]
                 a << sn << doc[sn]
               end
               [[meta, k], doc]
